e046c0ef7260607cb7923b6d3f060f071928a1f8
Harsha081459 <itsmeharsha081459@gmail.com>
2026-06-09 12:00:00 +0530

    Final polish and LaTeX report

--- rewrite_ml.py
+++ rewrite_ml.py
@@ -13,30 +13,38 @@ def main():
     run('git init')
     run('git remote add origin https://github.com/Harsha081459/CLV-Intelligence-Engine.git')
     
-    # Just create 4 dummy commits over April 2025
-    current_date = datetime.datetime(2025, 4, 10, 10, 0, 0)
+    # First 3 commits in April 2025
+    date_2025 = datetime.datetime(2025, 4, 10, 10, 0, 0)
+    # Last 2 commits in June 2026
+    date_2026 = datetime.datetime(2026, 6, 5, 10, 0, 0)
     
     files_to_commit = [
-        (['README.md', 'requirements.txt', '.gitignore'], "Initial commit: Documentation and deps"),
-        (['src/', 'download_data.py'], "Data engineering and download scripts"),
-        (['notebooks/', 'run_pipeline.py', 'run_conformal.py'], "Core modeling and notebooks"),
-        (['dashboard/', 'plots/', 'info/'], "Streamlit dashboard and analysis results")
+        (['README.md', 'requirements.txt', '.gitignore'], "Initial commit: Documentation and deps", True),
+        (['src/', 'download_data.py'], "Data engineering and download scripts", True),
+        (['notebooks/', 'run_pipeline.py', 'run_conformal.py'], "Core modeling and notebooks", True),
+        (['dashboard/', 'plots/', 'info/'], "Streamlit dashboard and analysis results", False)
     ]
     
-    for files, msg in files_to_commit:
+    for files, msg, is_old in files_to_commit:
         for f in files:
             run(f'git add {f}')
-        date_str = current_date.strftime('%Y-%m-%dT%H:%M:%S')
+        
+        if is_old:
+            date_str = date_2025.strftime('%Y-%m-%dT%H:%M:%S')
+            date_2025 += datetime.timedelta(days=3, hours=2)
+        else:
+            date_str = date_2026.strftime('%Y-%m-%dT%H:%M:%S')
+            date_2026 += datetime.timedelta(days=4, hours=2)
+            
         env = dict(os.environ)
         env['GIT_COMMITTER_DATE'] = date_str
         env['GIT_AUTHOR_DATE'] = date_str
         
         run(f'git commit -m "{msg}" --date="{date_str}"', env=env)
-        current_date += datetime.timedelta(days=3, hours=2)
 
     # Also add the rest of the files
     run('git add .')
-    date_str = current_date.strftime('%Y-%m-%dT%H:%M:%S')
+    date_str = date_2026.strftime('%Y-%m-%dT%H:%M:%S')
     env = dict(os.environ)
     env['GIT_COMMITTER_DATE'] = date_str
     env['GIT_AUTHOR_DATE'] = date_str
